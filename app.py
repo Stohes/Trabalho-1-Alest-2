ca8d934fdf90c6120b5865c292088c381e49f466
Stohes <T.Chies@edu.pucrs.br>
2022-03-20 16:56:03 -0300

    Versao 1 (10 minutos caso 2)

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
-
-#case = open("caso01.txt", "r")
-
-# print("teste")
-# print(case.read())
-
-
-# def dicCreator(file):  # ler o arquivo e criar o dic
-
-
+import datetime
+firstTime = datetime.datetime.now()
 with open("caso01.txt") as file:
     dic = {}
+    word = ""
+    counter = 1
     for line in file.readlines():
         try:
+            word += line.rsplit()[0]
             dic[line.rsplit()[0]] = line.rsplit()[1]
+            print("linha " + str(counter))
+            counter += 1
         except Exception as e:
             dic[line.rsplit()[0]] = ""
-        print(line)
-    print(dic.items())
+            print("exception " + str(counter))
+            counter += 1
+        # print(line)
+    index = 0
+    word2 = word
+    while index < len(word2):
+        if dic.get(word2[index]) != "":
+            word2 = word2.replace(word2[index], dic.get(word2[index]))
+        else:
+            index += 1
+with open("teste1.txt", "w") as file:
+    #file.write("tamanho: " + str(len(word2)))
+    file.write(word2)
+lastTime = datetime.datetime.now()
+difference = lastTime - firstTime
+# print(dic.items())
+# print(word)
+# print("")
+# print(word2)
+print(len(word2))
+print(difference.total_seconds())
+print("foi")
 
-    # botar readlines[0] na key e [1] no value
-    # dicCreator(case)
-    # def alg(word, dic):  # passar a palavra de teste e o dic
-    #    pass
+# 1:     268.297
+# 2: 598.497.341
