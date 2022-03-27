c40b79d7feb9fcd6a1829dd096ce7e48f9fcaba9
Stohes <T.Chies@edu.pucrs.br>
2022-03-26 23:14:22 -0300

    Solucao 1 nao otimizada

--- app.py
+++ app.py
@@ -1,39 +1,44 @@
 import datetime
 firstTime = datetime.datetime.now()
-with open("caso01.txt") as file:
+with open("caso02.txt") as file:
     dic = {}
     word = ""
-    counter = 1
     for line in file.readlines():
         try:
             word += line.rsplit()[0]
             dic[line.rsplit()[0]] = line.rsplit()[1]
-            print("linha " + str(counter))
-            counter += 1
         except Exception as e:
-            dic[line.rsplit()[0]] = ""
-            print("exception " + str(counter))
-            counter += 1
-        # print(line)
+            dic[line.rsplit()[0]] = line.rsplit()[0]
+
+    arr = dic.values()
+    for words in arr:
+        for char in words:
+            word = word.replace(char, "")
+
     index = 0
-    word2 = word
-    while index < len(word2):
-        if dic.get(word2[index]) != "":
-            word2 = word2.replace(word2[index], dic.get(word2[index]))
+# criar dicionario auxiliar com o head e o value do head
+    while index < len(word):
+        if dic.get(word[index]) != word[index]:
+            word = word.replace(word[index], dic.get(word[index]))
         else:
             index += 1
-with open("teste1.txt", "w") as file:
-    #file.write("tamanho: " + str(len(word2)))
-    file.write(word2)
+
 lastTime = datetime.datetime.now()
 difference = lastTime - firstTime
-# print(dic.items())
-# print(word)
-# print("")
-# print(word2)
-print(len(word2))
-print(difference.total_seconds())
+print("tamanho: " + str(len(word)))
+print("tempo: " + str(difference.total_seconds()))
 print("foi")
 
+
+# fazer letra por letra, por ex A vira mmmooomommmooommooommooommooomommmooommooommooo e nao memimomu FAZER ISSO 1
+
+# fazer um grafico da complexidade da solucao 1
+
+
+# solucao 1
 # 1:     268.297
 # 2: 598.497.341
+
+# solucao 2
+# 1:     178.833
+# 2: 399.461.207
